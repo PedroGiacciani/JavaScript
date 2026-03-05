bcfb2fa6b8e0b5459d2d51cf910a15e3c6537053
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-03-05 17:00:05 -0300

    ex016

--- Aulas/Aula 012 ex/ex016/script.js
+++ Aulas/Aula 012 ex/ex016/script.js
@@ -1,52 +1,58 @@
 function verificar(){
     var data = new Date()
-    var ano = data.getFullYear()    
-    var fano = document.getElementById('ianos')
+    var ano = data.getFullYear()
+    var fano = document.getElementById('iano')
     var res = document.getElementById('res')
 
     if(fano.value.length == 0 || fano.value > ano){
         window.alert('[ERRO] Verifique os dados e tente novamente !')
     } else{
-        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        var gênero = ''
+        var fsex = document.getElementsByName('radsex')
+        var gen = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
 
         if(fsex[0].checked){
-            gênero = 'Homem'
-            if(idade >= 0 && idade < 10){
-                //Criança
+            gen = 'Homem'
+            if(idade >= 0 && idade < 12){
+                //criança
                 img.setAttribute('src', 'Imagens/menino.png')
             } else if(idade < 21){
-                //Jovem
+                //jovem
                 img.setAttribute('src', 'Imagens/rapaz.png')
             } else if(idade < 50){
-                //Adulto
+                //adulto
                 img.setAttribute('src', 'Imagens/homem.png')
-            } else {
-                //Idoso
+            } else if (idade < 120){
+                //idoso
                 img.setAttribute('src', 'Imagens/idoso.png')
+            } else {
+                //morto
+                img.setAttribute('src', 'Imagens/velho.png')
             }
-
         } else {
-            gênero = 'Mulher'
-            if(idade >= 0 && idade < 10){
-                //Criança
+            gen = 'Mulher'
+            if(idade >= 0 && idade < 12){
+                //criança
                 img.setAttribute('src', 'Imagens/menina.png')
             } else if(idade < 21){
-                //Jovem
+                //jovem
                 img.setAttribute('src', 'Imagens/moca.png')
             } else if(idade < 50){
-                //Adulto
+                //adulto
                 img.setAttribute('src', 'Imagens/mulher.png')
-            } else {
-                //Idoso
+            } else if(idade < 120){
+                //idoso
                 img.setAttribute('src', 'Imagens/idosa.png')
+            } else {
+                //morto
+                img.setAttribute('src', 'Imagens/velho.png')
             }
         }
+
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.innerHTML = `Detectamos ${gen} com ${idade} anos`
         res.appendChild(img)
     }
 }
