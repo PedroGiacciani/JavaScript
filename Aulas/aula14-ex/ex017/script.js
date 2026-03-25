334639365117474659f9af27a363f282b2b6fc67
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-03-25 16:16:55 -0300

    update nos exercicios

--- Aulas/aula14-ex/ex017/script.js
+++ Aulas/aula14-ex/ex017/script.js
@@ -1,23 +1,28 @@
 function contar(){
+    //Variavéis necessárias
     var c = Number(document.getElementById('ipri').value)
     var f = Number(document.getElementById('ifim').value)
     var p = Number(document.getElementById('ipas').value)
-    var res = document.getElementById('res')
 
-    if(c == '' || f == ''){
-        res.innerHTML = `Operação cancelada`
-    } else if(p == 0 || p == ''){
-        alert('Passo inválido. Passo será considerado 1')
-        p = 1
-        res.innerHTML = ``
-        for(c; c <= f; c += p){
-            res.innerHTML += `--> ${c}`
-        }
+    //Verificar se os dados são suficientes
+    if(c == '' || f == ''){ 
+        res.innerHTML = 'Impossível contar !'
+        alert("[ERRO] faltam dados")
     } else{
-        res.innerHTML = ``
-        for(c; c <= f; c += p){
-            res.innerHTML += `--> ${c}`
+        res.innerHTML = `Contando: <br>`
+        if(p == 0 || p == ''){
+            res.innerHTML = `Passo inválido! Considerando PASSO 1 <br>`
+            p = 1
         }
-        res.innerHTML += ' emoji'
+        if(c > f){
+            for(c; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else{
+            for(c; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += '\u{1F3C1}'
     }
 }
--- Aulas/aula14-ex/ex018/script.js
+++ Aulas/aula14-ex/ex018/script.js
@@ -1,12 +1,15 @@
 function verificar(){
     n = Number(document.getElementById('inum').value)
-    var res = document.getElementById('ires')
+    var res = document.getElementById('seltab')
 
     if(n != ''){
         res.innerHTML = ``
         for(var m = 0; m <= 10; m++){
             var s = n * m
-            res.innerHTML += `${n} X ${m} = ${s} <br>`
+            var item = document.createElement('option')
+            item.text = `${n} X ${m} = ${s}`
+            item.value = `tab${m}` 
+            res.appendChild(item)
         }
     } else{
         alert('[ERRO] Digite um número válido')
