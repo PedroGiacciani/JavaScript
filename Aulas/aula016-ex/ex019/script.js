2cfb9c0c848e64c6fcecea8af578d69f1f131053
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-04-08 15:42:59 -0300

    update

--- Aulas/aula016-ex/ex019/script.js
+++ Aulas/aula016-ex/ex019/script.js
@@ -13,4 +13,16 @@ function adicionar(){
     sel.appendChild(op)
     a.push(num)
     op.innerHTML = `Valor ${num}`
-}   
+}
+
+function verificar(){
+    var res = document.getElementById('res')
+    var soma = 0;
+    a.sort()
+    
+    for(var i = 0; i <= a.length - 1; i++){
+        soma = a[i] + a[i + 1]
+    }
+
+    res.innerHTML = `Ao todo, temos ${a.length} números cadastrados <br> O maior valor informado foi: ${a[a.length - 1]} <br> O menor valor informado foi ${a[0]} <br> Somando todos os valores, temos: ${soma} <br> A média dos valores digitados é ${soma/a.length}`        
+}
