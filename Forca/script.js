83637831b893fdc02663940002ec7cc90fca1af7
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-04-21 01:25:42 -0300

    Parte de chutes concluída

--- Forca/script.js
+++ Forca/script.js
@@ -1,15 +1,29 @@
 let teste = document.querySelector('div#letras')
 let txt = document.getElementById('itxt')
+let letras = []
 
 function validar(){
     let palavra = String(txt.value)
-    let letras = palavra.split('')
-    
+    letras = palavra.split('')
+
     for(let pos in letras){
         let caixa = document.createElement('p')
+        caixa.setAttribute('id', pos)
         caixa.setAttribute('class', 'caixa')
         let container = document.querySelector('div#letras')
         container.appendChild(caixa)
     }
     txt.value = ``
+}
+
+function responder(){
+    let c = document.querySelector('input#ires')
+    let erros = document.getElementById('erros')
+    let chute = String(c.value)
+    if(letras.indexOf(chute) != -1){
+        let l = document.getElementById(letras.indexOf(chute))
+        l.innerHTML = `${letras[letras.indexOf(chute)]}`
+    } else{
+        erros.innerHTML += `${chute}, `
+    }
 }
