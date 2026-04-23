d87aa0d2e907a2ac7879c80a6b6b5b32224738ba
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-04-23 01:04:30 -0300

    Simulador de pedidos com settimeout

--- Projetos/Pedidos/script.js
+++ Projetos/Pedidos/script.js
@@ -1,12 +1,16 @@
 let pizza = document.getElementById('ipizza')
-let saborPizza = String(pizza.value)
+let nome = document.getElementById('inome')
 let res = document.querySelector('div#res')
-let botao = document.getElementById('botao')
-botao.addEventListener('click', prepararPizza(saborPizza, pronto(saborPizza)))
 
-function prepararPizza(sabor, callback){
-    res.innerHTML = `Estamos preparando sua pizza de ${sabor}. Aguarde...`
-    setTimeout(() => callback(sabor), 2000);
+function chamar(){
+    let saborPizza = String(pizza.value)
+    let nomePedido = String(nome.value)
+    if(saborPizza.length == 0 || nomePedido.length == 0){
+        alert("Preencha todos os campos!")
+    }else{
+        res.innerHTML = `Olá ${nomePedido}, seu pedido de pizza de ${saborPizza} foi recebido!. Aguarde enquanto preparamos...`
+        setTimeout(() => pizzaPronta(nomePedido, saborPizza),4000);
+    }
 }
 
-const pronto = (sabor) => res.innerHTML = `Prontinho, pizza de ${sabor} pronta`
+const pizzaPronta = (nome, sabor) => res.innerHTML = `Pronto ${nome}, sua pizza de ${sabor} está pronta!!`
