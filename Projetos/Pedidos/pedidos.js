2171ca1aab3f7fbcdba0322001eb30e1149fb677
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-04-27 15:05:14 -0300

    Update pedidos.js

--- Projetos/Pedidos/pedidos.js
+++ Projetos/Pedidos/pedidos.js
@@ -7,6 +7,20 @@ function chamar(nome, sabor, callback){
     }, 1000)
 }
 
+function chamada(nome, sabor, c1, c2, c3, c4){
+    c1(nome, sabor)
+    setTimeout(() => {
+        c2(nome, sabor)
+        setTimeout(() => {
+            c3(nome, sabor)
+            setTimeout(() => {
+                c4(nome)
+            
+            }, 1500)
+        }, 1500)
+    }, 1500)
+}
+
 const recebido = (nome, sabor) => console.log(`Olá ${nome}, seu pedido de pizza de ${sabor} foi registrado e está sendo preparado!!`)
 const preparando = (nome, sabor) => console.log(`${nome}, sua pizza de ${sabor} está sendo preparada...`)
 const pronto = (nome, sabor) => console.log(`${nome}, sua pizza de ${sabor} está pronta!!`)
@@ -17,22 +31,3 @@ chamar(nome, sabor, chamada)
 // chamar('Amanda', 'Frango com catupiry', pronto, entrega)
 // chamar('Nilo', 'Pepperoni', pronto, entrega)
 // chamar('Robson', 'Mussarela', pronto, entrega)
-
-// function chamada(nome, sabor, c1, c2, c3, c4){
-//     c1(nome, sabor)
-
-//     setTimeout(() => {
-//         c2(nome, sabor)
-
-//         setTimeout(() => {
-//             c3(nome, sabor)
-
-//             setTimeout(() => {
-//                 c4(nome)
-                
-//             }, 1500)
-
-//         }, 1500)
-
-//     }, 1500)
-// }
