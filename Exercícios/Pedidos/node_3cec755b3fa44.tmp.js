a4e4efccefa0375613c375854cd7e03507923848
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-05-05 14:16:07 -0300

    Create node_3cec755b3fa44.tmp.js

---
+++ Exercícios/Pedidos/node_3cec755b3fa44.tmp.js
@@ -0,0 +1,72 @@
+let estoque = ['tomate', 'cebola', 'pimentão', 'alface', 'cebolinha']
+let item = 'tomate'
+let nome = 'Pedro'
+
+function validarPedido(item){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(estoque.includes(item)){
+                console.log('Produto em estoque')
+                resolve()
+            } else{
+                reject(`Produto indisponível`)
+            }
+        }, 1000);
+    })
+}
+
+function processarPagamento(nome){
+    let verificarPagamento = Math.random() < 0.7
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(verificarPagamento == true){
+                console.log(`${nome}, Pagamento realizado!`)
+                resolve()
+            } else{
+                reject(`Pagamento recusado`)
+            }
+        }, 1000);
+    })
+}
+
+function preparandoPedido(item){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`Pedido de ${item} realizado!`)
+            resolve()
+        }, 2000)
+    })
+}
+
+function entregarPedido(nome, item){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`${nome}, seu pedido de ${item} foi entregue!!`)
+            resolve()
+        }, 3000)
+    })
+}
+
+async function tentarPagamento(nome){
+    try{
+        await processarPagamento(nome)
+    } catch(erro){
+        console.log(`[ERRO] ${erro}, tentando novamente...`)
+        await processarPagamento(nome)
+    }
+}
+
+async function pedido(item, nome){
+    try{
+        await validarPedido(item)
+        await tentarPagamento(nome)
+        await preparandoPedido(item)
+        await entregarPedido(nome, item)
+        console.log(`Obrigado pela preferência!`)
+
+    } catch(erro){
+        console.log(`[ERRO] ${erro}`)
+    }
+}
+
+pedido(item, nome)
