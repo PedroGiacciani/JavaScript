c4a9f27e113d4b3b6bf1f1f14dd2cb2429aaa83a
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-05-13 01:20:45 -0300

    Biblioteca com sistema de Object Array

--- Exercícios/biblioteca/biblioteca.js
+++ Exercícios/biblioteca/biblioteca.js
@@ -1,6 +1,8 @@
-let nomeLivro = 'A arte da Guerra'
+//Declaração de Variáveis
+let nomeLivro = 'A Arte da Guerra'
 let usuario = 'Pedro'
 
+//Declaração da Classe Livro
 class Livro{
     constructor(nome, autor, disponibilidade){
         this.nome = nome
@@ -13,47 +15,51 @@ class Livro{
     }
 }
 
+//Declaração da Object Array
 let livros = [
-    new Livro('Harry Potter 1', 'J. K. Rolling', true),
-    new Livro('Fazbear Frights 2: Fetch', 'Scott Cawthon', true),
-    new Livro('Alice no País das Maravilhas', 'Lewis Carol', false),
-    new Livro('Cartas de um Diabo ao seu aprendiz', 'C. S. Lewis', true),
-    new Livro('A arte da Guerra', 'Shun Tzu', false)
+    new Livro('Harry Potter 1', 'J. K. Rowling', true),
+    new Livro('Alice no País das Maravilhas', 'Lewis Carrol', true),
+    new Livro('Fnaf: Silver Eyes', 'Scott Cawthon', false),
+    new Livro('Cartas de um Diabo ao seu Aprendiz', 'C. S. Lewis', false),
+    new Livro('A Arte da Guerra', 'Sun Tzu', true)
 ]
 
 let livrosDisponiveis = livros.filter(pos => pos.disponibilidade == 'Disponível')
 
 function isAvaible(nomeLivro){
-    if(livrosDisponiveis.find(pos => pos.nome == nomeLivro)){
-        return true
-    } else{
-        return false
-    }
+    let livroEscolhido = livrosDisponiveis.find(pos => pos.nome == nomeLivro)
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(livroEscolhido){
+                console.log(`Seu livro: ${nomeLivro} está sendo preparado...`)
+                resolve()
+            }else{
+                reject(`Sentimos muito, seu livro: ${nomeLivro} está indisponível`)
+            }
+        }, 2000)
+    })
 }
 
-function emprestarLivro(usuario){
+function prepararLivro(nomeLivro, usuario){
     let livroEscolhido = livrosDisponiveis.find(pos => pos.nome == nomeLivro)
     return new Promise((resolve) => {
         setTimeout(() => {
             livroEscolhido.disponibilidade = false
             livrosDisponiveis = livros.filter(pos => pos.disponibilidade == 'Disponível')
-            console.log(`Livro emprestado para ${usuario}`)
+            console.log(`Livro: ${nomeLivro}, emprestado para ${usuario}`)
             resolve()
-        }, 2000)
+        }, 4000)  
     })
 }
 
-async function pegarLivro(nomeLivro, usuario){
+async function emprestarLivro(nomeLivro, usuario){
     try{
-        if(isAvaible(nomeLivro) == true){
-            console.log(`Olá ${usuario}, o livro ${nomeLivro} está disponível.\nAguarde 2 segundos enquanto preparamos ele para você`)
-            await emprestarLivro(usuario)
-        } else{
-            throw new Error('Livro indisponível')
-        }
+        console.log(`Olá ${usuario}, agradecemos a preferência!!`)
+        await isAvaible(nomeLivro)
+        await prepararLivro(nomeLivro, usuario)
     } catch(err){
-        console.log(`${err}`)
+        console.log(`[ERRO]: ${err}`)
     }
 }
 
-pegarLivro(nomeLivro, usuario)
+emprestarLivro(nomeLivro, usuario)
