b4a3cdb8a8a3133af7375f479bb08863f4240da6
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-05-14 16:16:53 -0300

    Update biblioteca2.js

--- Exercícios/biblioteca/biblioteca2.js
+++ Exercícios/biblioteca/biblioteca2.js
@@ -9,6 +9,27 @@ class Livro{
         this.autor = autor
         this.disponibilidade = disponibilidade
     }
+
+    emprestarLivro(usuario){
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                console.log(`Agradecemos a preferência em alugar um livro conosco!!`)
+                if(this.disponibilidade == true){
+                    this.disponibilidade = false
+                    console.log(`Livro ${this.nome} emprestado para ${usuario}`)
+                    resolve()
+                } else if(this.disponibilidade == false){
+                    this.devolverLivro(usuario)
+                    resolve()
+                }
+            }, 2000);
+        })    
+    }
+
+    devolverLivro(usuario){
+        this.disponibilidade = true
+        console.log(`Livro: ${this.nome} devolvido por ${usuario}`)
+    } 
 }
 
 //Declaração da Object Array
@@ -19,3 +40,17 @@ let livros = [
     new Livro('Cartas de um Diabo ao seu Aprendiz', 'C. S. Lewis', false),
     new Livro('A Arte da Guerra', 'Sun Tzu', true)
 ]
+
+let livroEscolhido = livros.find(pos => pos.nome == nomeLivro)
+
+
+async function interagirLivro(usuario){
+    console.log(`Olá ${usuario},`)
+    if(livroEscolhido != undefined){
+        await livroEscolhido.emprestarLivro(usuario)
+    } else{
+        console.log(`Sentimos muito, parece que o livro que procura está indisponível`)
+    }
+}
+
+interagirLivro(usuario)
