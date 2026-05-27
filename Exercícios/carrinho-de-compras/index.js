953471cc977729840186d26202bb32031a9a6be7
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-05-27 12:37:48 -0300

    Atualização sobre quantidade de produtos no carrinho

--- Exercícios/carrinho-de-compras/index.js
+++ Exercícios/carrinho-de-compras/index.js
@@ -1,9 +1,8 @@
 class Produto{
-    constructor(nome, preco, estoque, quantidade){
+    constructor(nome, preco, estoque){
         this.nome = nome
         this.preco = preco
         this.estoque = estoque
-        this.quantidade = quantidade
     }
 }
 
@@ -12,29 +11,31 @@ class Carrinho{
         this.produtos = []
     }
 
-    adicionarProduto(nomeProduto){
-        if(nomeProduto.estoque > 0){
-            this.produtos.push(nomeProduto)
-            nomeProduto.estoque -= 1
-        }else{
-            console.log(`${nomeProduto.nome} fora de estoque`)
+    adicionarProduto(item, quantidade){
+        let falta = (quantidade - item.produto.estoque) 
+        for(let i = 0; i < quantidade && item.produto.estoque > 0; i++){
+            this.produtos.push(item)
+            item.produto.estoque -= 1
+        }
+        if(item.produto.estoque <= 0){
+            console.log(`${falta} ${item.produto.nome}(s) fora de estoque`)
         }
     }
 
-    removerProduto(nomeProduto){
-        let index = this.produtos.indexOf(nomeProduto)
+    removerProduto(item){
+        let index = this.produtos.indexOf(item)
         if(index != -1){
             this.produtos.splice(index, 1)
-            nomeProduto.estoque += 1
+            item.produto.estoque += 1
         }
     }
 
     calcularTotal(){
-        return this.produtos.reduce((acc, cur) => acc + cur.preco, 0)  
+        return this.produtos.reduce((acc, cur) => acc + cur.produto.preco * cur.quantidade, 0)  
     }
 
     listarProdutos(){
-        return this.produtos.map(pos => pos.nome)
+        return this.produtos.map(pos => pos.produto.nome)
     }
 }
 
@@ -44,14 +45,27 @@ const coca = new Produto('coca-cola', 10.00, 3)
 const picanha = new Produto('picanha', 69.90, 10)
 const sabao = new Produto('sabão em pó', 11.00, 6)
 
-const produtos = [coca, tomate, picanha, coca, coca, coca]
+const produtos = [
+    {
+        produto: coca,
+        quantidade: 4
+    },
+    {
+        produto: tomate,
+        quantidade: 1
+    },
+    {
+        produto: picanha,
+        quantidade: 21
+    },
+]
 
 const carrinho = new Carrinho()
-const addProdutos = (produtos) => produtos.forEach(pos => carrinho.adicionarProduto(pos))
+const addProdutos = (produtos) => produtos.forEach(pos => carrinho.adicionarProduto(pos, pos.quantidade))
 const verificarEstoque = (produtos) => {
     return new Promise((resolve, reject) =>{
         setTimeout(() => {
-            if(produtos.every(pos => pos.estoque > 0) == true){
+            if(produtos.every(pos => pos.produto.estoque > 0) == true){
                 resolve(`Produtos disponíveis e adicionados ao carrinho!`)
             }else{
                 reject(`Produtos indisponíveis!`)
@@ -67,6 +81,7 @@ async function finalizarCompra(produtos){
         console.log(msg)
         addProdutos(produtos)
         console.log(carrinho.listarProdutos())
+        console.log(produtos)
         const valorFinal = carrinho.calcularTotal()
         console.log(`A compra deu: R$${valorFinal}`)
     }catch(err){
