65ccd8e7e5a8b81559f91695adfc9b8850b76392
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-08-09 20:47:41 -0300

    Atualização

--- Exercícios/lista-tarefas/scripts/app.js
+++ Exercícios/lista-tarefas/scripts/app.js
@@ -1,17 +1,17 @@
-import { Tarefa } from "./tarefas.js";
+import { Tarefa, adicionarTarefa } from "./tarefas.js";
 
-let novaTarefa = document.getElementById('itarefa')
+let campoTarefa = document.getElementById('icampoTarefa')
 let btnTarefa = document.getElementById('btn-tarefa')
-let campoTarefas = document.getElementById('lista-tarefas')
+let areaTarefa = document.getElementById('lista-tarefas')
+
 let agora = new Date()
 let agoraBr = agora.toLocaleDateString('pt-BR')
+
 let listaTarefas = JSON.parse(localStorage.getItem('bancoTarefas')) || []
-let id = 0
 
-btnTarefa.addEventListener('click', () => {
-    const tarefa = new Tarefa(Math.floor(Math.random() * 11111), novaTarefa.value, false, agoraBr)
-    listaTarefas.push(tarefa)
-    localStorage.setItem('bancoTarefas', JSON.stringify(listaTarefas))
-})
-
-campoTarefas.innerHTML += `<p>${listaTarefas}</p>`
+btnTarefa.addEventListener('click', () => adicionarTarefa(campoTarefa.value, agoraBr, listaTarefas, areaTarefa))
+campoTarefa.addEventListener('keypress', (event) => {
+    if(event.key == 'Enter'){
+        adicionarTarefa(campoTarefa.value, agoraBr, listaTarefas, areaTarefa)
+    }
+})  
--- Exercícios/lista-tarefas/scripts/tarefas.js
+++ Exercícios/lista-tarefas/scripts/tarefas.js
@@ -7,3 +7,9 @@ export class Tarefa{
     }
 }
 
+export function adicionarTarefa(titulo, dataCriacao, listaTarefas, areaTarefa){
+    const tarefa = new Tarefa(Math.floor(Math.random() * 1111), titulo, false, dataCriacao)
+    listaTarefas.push(tarefa)
+    localStorage.setItem('bancoTarefas', JSON.stringify(listaTarefas))
+    areaTarefa.innerHTML += `<p>${tarefa.id} ${tarefa.titulo} ${tarefa.concluida} ${tarefa.dataCriacao} <button><i class="fa-solid fa-pencil"></i></button><button><i class="fa-solid fa-trash"></i></button></p>`
+}
