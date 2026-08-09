724c47bcaead9045f620a36d98a86e61bcb8c1ca
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-08-09 18:57:10 -0300

    Atualização

--- Exercícios/lista-tarefas/scripts/app.js
+++ Exercícios/lista-tarefas/scripts/app.js
@@ -6,10 +6,10 @@ let campoTarefas = document.getElementById('lista-tarefas')
 let agora = new Date()
 let agoraBr = agora.toLocaleDateString('pt-BR')
 let listaTarefas = JSON.parse(localStorage.getItem('bancoTarefas')) || []
+let id = 0
 
 btnTarefa.addEventListener('click', () => {
-    let id = 0
-    const tarefa = new Tarefa(id + 1, novaTarefa.value, false, agoraBr)
+    const tarefa = new Tarefa(Math.floor(Math.random() * 11111), novaTarefa.value, false, agoraBr)
     listaTarefas.push(tarefa)
     localStorage.setItem('bancoTarefas', JSON.stringify(listaTarefas))
 })
