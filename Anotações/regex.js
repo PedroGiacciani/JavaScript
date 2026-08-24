6a40154dfffb97a129e0268a36bacc9991bf358a
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-08-23 22:35:29 -0300

    Update regex.js

--- Anotações/regex.js
+++ Anotações/regex.js
@@ -17,6 +17,19 @@ console.log(numeros.match(/10*/g)) //Retorna todas as sentenças que tenham ou 1
 console.log(numeros.match(/10?/g)) //Retorna todos os 1 ou 10
 
 //Ancoras]
-n = '1209, 1823 adada'
-const re = /^[0-9]|^[a-z]+$/
-console.log(re.test(n))
+n = '401.815.588-08'
+const re = /[0-9]{3}[.][0-9]{3}[.][0-9]{3}[-][0-9]{2}/ //Validar formato CPF
+const re2 = /[0-9]{2}$/ //Teste com $ e ^
+console.log(re2.test(n))
+
+let data = '2026/23/08'
+const reData = /([0-9]{4})\/([01][0-9])\/([0-9]{2})/
+
+let resultado = data.match(reData)
+
+if(resultado){
+    novaData = data.replace(reData, '$3/$2/$1')
+    console.log(novaData)
+}else{
+    console.log('ok')
+}
